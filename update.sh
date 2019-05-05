d8f25ace3908538524d8114b94b042eb65d14bcd
Jun Aruga <happy@oh4u.net>
2019-05-06 00:31:24 +0200

    Add new version CentOS 7.6 dropping old versions
    * Comment out cases that create invalid container images.
    * We do not keep Dockerfile files on the repository, as those are generated in update.sh.
    * Install libguestfs-tools to use virt-tar-out command from xenial repo.
      We can not install it from bionic repo because of below error.
    
      The following packages have unmet dependencies:
      libguestfs-tools : Depends: libguestfs0 (= 1:1.36.13-1ubuntu3) but it is not going to be installed
                         Depends: libguestfs-perl (= 1:1.36.13-1ubuntu3) but it is not going to be installed
      E: Unable to correct problems, you have held broken packages.
    * Update qemu v3.1.0-3.
    * Rafactoring mixed indent with spaces and tab, replacing tab to spaces.

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -xeo pipefail
 
 cd "$(readlink -f "$(dirname "$BASH_SOURCE")")"
 
@@ -14,17 +14,17 @@ get_part() {
     part="$1"
     shift
     if [ -f "$dir/$part" ]; then
-	cat "$dir/$part"
-	return 0
-	fi
+        cat "$dir/$part"
+        return 0
+    fi
     if [ -f "$part" ]; then
-	cat "$part"
-	return 0
-	fi
+        cat "$part"
+        return 0
+    fi
     if [ $# -gt 0 ]; then
-	echo "$1"
-	return 0
-	fi
+        echo "$1"
+        return 0
+    fi
     return 1
 }
 
@@ -47,34 +47,58 @@ for version in "${versions[@]}"; do
     mkdir -p iso-slim
 
     # create rootfs.tar
-    if [ "$(echo *.img.xz)" != "*.img.xz" ]; then
-	imgxzfilename="$(echo *.img.xz)"
-	imgfilename="${imgxzfilename%.xz}"
+    # Minimaize used sudo scope.
+    # virt-tar-out and guestfish outputs below meesage without sudo.
+    # "*stdin*:1: libguestfs: error: /usr/bin/supermin exited with error status 1"
+    # https://bugzilla.redhat.com/show_bug.cgi?id=1591617
+    # That's a bug of Ubuntu kernel. This does not happen on Fedora.
+    # https://bugs.launchpad.net/ubuntu/+source/linux/+bug/759725
+    if [[ $url =~ \.(img|raw)\.xz$ ]]; then
+        imgxzfilename="$(basename $url)"
+        imgfilename="${imgxzfilename%.xz}"
 
-	if [ ! -f "${imgfilename}" ]; then
-	    cat "${imgxzfilename}" | unxz > "${imgfilename}"
-	fi
-	if [ ! -f iso-slim/rootfs.tar ]; then
-	    virt-tar-out -a "${imgfilename}" / - > iso-slim/rootfs.tar
-	fi
+        if [ ! -f "${imgfilename}" ]; then
+            cat "${imgxzfilename}" | unxz > "${imgfilename}"
+        fi
+        if [ ! -f iso-slim/rootfs.tar ]; then
+            sudo virt-tar-out -a "${imgfilename}" / iso-slim/rootfs.tar
+            user_name=$(id -un)
+            group_name=$(id -gn)
+            sudo chown ${user_name}:${group_name} iso-slim/rootfs.tar
+        fi
     fi
-    if [ "$(echo *.tar.xz)" != "*.tar.xz" ]; then
-	xzfilename="$(echo *.tar.xz)"
-	if [ ! -f iso-slim/rootfs.tar ]; then
-	    cat "${xzfilename}" | unxz > iso-slim/rootfs.tar
-	fi
+    if [[ $url =~ \.tar\.xz$ ]]; then
+        xzfilename="$(basename $url)"
+        if [ ! -f iso-slim/rootfs.tar ]; then
+            cat "${xzfilename}" | unxz > iso-slim/rootfs.tar
+        fi
     fi
-    if [ "$(echo *.qcow2)" != "*.qcow2" ]; then
-	qcow2filename="$(echo *.qcow2)"
-	if [ ! -f iso-slim/rootfs.tar ]; then
-	    virt-tar-out -a "${qcow2filename}" / - > iso-slim/rootfs.tar
-	fi
+    if [[ $url =~ \.qcow2$ ]]; then
+        filename="$(basename $url)"
+        if [ ! -f iso-slim/rootfs.tar ]; then
+            sudo virt-tar-out -a "${filename}" / iso-slim/rootfs.tar
+            user_name=$(id -un)
+            group_name=$(id -gn)
+            sudo chown ${user_name}:${group_name} iso-slim/rootfs.tar
+        fi
     fi
-    if [ "$(echo *.iso)" != "*.iso" ]; then
-	isofilename="$(echo *.iso)"
-	if [ ! -f iso-slim/rootfs.tar ]; then
-	    virt-tar-out -a "${isofilename}" / - > iso-slim/rootfs.tar
-	fi
+    if [[ $url =~ \.iso$ ]]; then
+        filename="$(basename $url)"
+        if [ ! -f iso-slim/rootfs.tar ]; then
+            # Use guestfish directly because of virt-tar-out issue for iso file.
+            # https://github.com/libguestfs/libguestfs/issues/37
+            # https://www.redhat.com/archives/libguestfs/2019-May/msg00019.html
+            file_system=$(
+                sudo guestfish -a ${filename} --ro <<EOF | cut -d: -f 1
+                run
+                list-filesystems
+EOF
+)
+            sudo guestfish --ro -a "${filename}" -m "${file_system}" tar-out / iso-slim/rootfs.tar
+            user_name=$(id -un)
+            group_name=$(id -gn)
+            sudo chown ${user_name}:${group_name} iso-slim/rootfs.tar
+        fi
     fi
 
     # create iso-slim dockerfile
@@ -88,31 +112,38 @@ EOF
     ## build iso-slim image
     docker build -t $repo:$version-iso-slim iso-slim
     for tag in $tags; do
-    	docker tag $repo:$version-iso-slim $repo:$tag-iso-slim
+        docker tag $repo:$version-iso-slim $repo:$tag-iso-slim
     done
 
     # create iso dockerfile
     mkdir -p iso
     if [ -n "${qemu_arch}" -a ! -f "iso/qemu-${qemu_arch}-static" ]; then
-	wget https://github.com/multiarch/qemu-user-static/releases/download/v2.9.1/qemu-${qemu_arch}-static -O "iso/qemu-${qemu_arch}-static"
-	chmod +x "iso/qemu-${qemu_arch}-static"
+        wget https://github.com/multiarch/qemu-user-static/releases/download/v3.1.0-3/qemu-${qemu_arch}-static -O "iso/qemu-${qemu_arch}-static"
+        chmod +x "iso/qemu-${qemu_arch}-static"
     fi
     if [ -n "${qemu_arch}" ]; then
-	cat > iso/Dockerfile <<EOF
+        cat > iso/Dockerfile <<EOF
 FROM $repo:$version-iso-slim
 ADD qemu-${qemu_arch}-static /usr/bin
 EOF
     else
-	cat > iso/Dockerfile <<EOF
+        cat > iso/Dockerfile <<EOF
 FROM $repo:$version-iso-slim
 EOF
     fi
     docker build -t $repo:$version-iso iso
     for tag in $tags; do
-	docker tag $repo:$version-iso $repo:$tag-iso
+        docker tag $repo:$version-iso $repo:$tag-iso
     done
 
-    docker run -it --rm $repo:$version-iso uname -a
+    docker run -it --rm $repo:$version-iso bash -xc '
+        uname -a
+        echo
+        cat /etc/os-release 2>/dev/null
+        echo
+        cat /etc/redhat-release 2>/dev/null
+        true
+    '
 
     # build iso-cleanup image
     mkdir -p iso-clean
@@ -132,12 +163,19 @@ EOF
     tmpname=export-$(openssl rand -base64 10 | sed 's@[=/+]@@g')
     docker run --name="$tmpname" --entrypoint=/does/not/exist tmp-$repo:$version-iso-cleaner 2>/dev/null || true
     docker export "$tmpname" | \
-	docker import \
-	       -c "ENV ARCH=${scw_arch} CENTOS_VERSION=${centos_version} DOCKER_REPO=${repo} CENTOS_IMAGE_URL=${url} QEMU_ARCH=${qemu_arch}" \
-	       - "$repo:$version-clean"
+        docker import \
+           -c "ENV ARCH=${scw_arch} CENTOS_VERSION=${centos_version} DOCKER_REPO=${repo} CENTOS_IMAGE_URL=${url} QEMU_ARCH=${qemu_arch}" \
+           - "$repo:$version-clean"
     docker rm "$tmpname"
     for tag in $tags; do
-	docker tag $repo:$version-clean $repo:$tag-clean
+        docker tag $repo:$version-clean $repo:$tag-clean
     done
-    docker run -it --rm $repo:$version-clean uname -a
+    docker run -it --rm $repo:$version-clean bash -xc '
+        uname -a
+        echo
+        cat /etc/os-release 2>/dev/null
+        echo
+        cat /etc/redhat-release 2>/dev/null
+        true
+    '
 done
